9b921973598f0e2500f8bf09ef0dc1ec2081949c
baonq0507 <nquangbao0507@gmail.com>
2024-11-26 04:37:08 +0700

    develop

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -352,6 +352,7 @@ class ProductSeeder extends Seeder
             file_put_contents($path, file_get_contents($url));
             $path = '/images/products/' . $filename;
         }
+        $path = str_replace(public_path(), '', $path);
         return $path;
     }
 }
